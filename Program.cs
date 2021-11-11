b704b61bb548c64a323b961f450e6a15325d0c7c
Utilizador <Utilizador@DESKTOP-LD5MLEL>
2021-11-11 18:08:30 +0000

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tetse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
